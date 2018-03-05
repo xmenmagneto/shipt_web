d872df4e1e1cbad9fae153e62ead3e80a9daff6d
Tingchang Deng <tingchangdeng@Tingchangs-MacBook-Pro.local>
2018-03-05 15:32:40 -0800

    change APT_ROOT to cloud server

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
-export const API_ROOT = 'http://localhost:8080';
+export const API_ROOT = 'https://shipt-197015.appspot.com';
 export const TOKEN_KEY = 'TOKEN_KEY';
 export const AUTH_PREFIX = 'Bearer';
